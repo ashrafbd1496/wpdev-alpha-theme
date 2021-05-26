a32ba3b6ec8abe0cbbe20b21938958459c14ca6a
ashrafuddin1496 <asraf1496@gmail.com>
2021-05-26 13:07:47 +0600

    site url added into title

---
+++ footer.php
@@ -0,0 +1,12 @@
+<div class="footer">
+    <div class="container">
+        <div class="row">
+            <div class="col-md-12">
+                &copy; <a href="https://ashrafbd.com">Ashrafbd</a> - All Rights Reserved
+            </div>
+        </div>
+    </div>
+</div>
+<?php wp_footer();   ?>
+</body>
+</html>
