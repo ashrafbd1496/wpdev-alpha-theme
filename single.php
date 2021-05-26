6b27fdab324d7fb5d14832574a124f7af6f2ccd7
ashrafuddin1496 <asraf1496@gmail.com>
2021-05-26 22:02:56 +0600

    sidebar widget created and has made dynamic

--- single.php
+++ single.php
@@ -3,76 +3,84 @@
 <body <?php body_class(); ?>>
 
 <?php get_template_part('hero');?>
+<div class="container">
+    <div class="row">
+        <div class="col-md-8">
+            <div class="posts">
+                <?php while (have_posts()){
+                    the_post();
+                    ?>
+                    <div class="post" <?php post_class(); ?>>
+                        <div class="container">
+                            <div class="row">
+                                <div class="col-md-12">
+                                    <h2 class="post-title">
+                                        <?php the_title(); ?>
+                                    </h2>
+                                </div>
+                            </div>
+                            <div class="row">
+                                <div class="col-md-4">
+                                    <p>
+                                        <strong>
+                                            <?php the_author(); ?>
+                                        </strong><br/>
+                                        <?php echo get_the_date();?>
+                                    </p>
+                                    <?php echo get_the_tag_list("<ul class='list-unstyled'><li>","</li><li>","</li></ul>"); ?>
 
-<div class="posts">
+                                </div>
+                                <div class="col-md-8">
+                                    <p>
+                                        <?php
+                                        if ( has_post_thumbnail() ) {
+                                            the_post_thumbnail('large', array('class' => 'img-fluid'));
 
-    <?php while (have_posts()){
-        the_post();
-        ?>
+                                        }
+                                        ?>
+                                    </p>
+                                    <?php
+                                    the_content();
 
-        <div class="post" <?php post_class(); ?>>
-            <div class="container">
-                <div class="row">
-                    <div class="col-md-12">
-                        <h2 class="post-title text-center">
-                            <?php the_title(); ?>
-                        </h2>
+                                    next_post_link();
+                                    echo '</br>';
+                                    previous_post_link();
+                                    ?>
+
+                                </div>
+                                <?php if (comments_open()): ?>
+                                    <div class="col-md-8">
+                                        <?php comments_template(); ?>
+                                    </div>
+                                <?php endif; ?>
+                            </div>
+
+                        </div>
                     </div>
-                </div>
-                <div class="row">
-                    <div class="col-md-4">
-                        <p>
-                            <strong>
-                                <?php the_author(); ?>
-                            </strong><br/>
-                            <?php echo get_the_date();?>
-                        </p>
-                        <?php echo get_the_tag_list("<ul class='list-unstyled'><li>","</li><li>","</li></ul>"); ?>
 
-                    </div>
-                    <div class="col-md-8">
-                        <p>
+                    <?php
+                } ?>
+
+                <div class="container post-paginate">
+                    <div class="row">
+                        <div class="col-md-4"></div>
+                        <div class="col-md-8">
                             <?php
-                            if ( has_post_thumbnail() ) {
-                                the_post_thumbnail('large', array('class' => 'img-fluid'));
-
-                            }
+                            the_posts_pagination(array(
+                                'screen_reader_text'    => ' ',
+                                'prev_text' =>'New Posts',
+                                'next_text' =>'Old Posts'
+                            ));
                             ?>
-                        </p>
-                        <?php
-                        the_content();
-
-                        next_post_link();
-                        echo '</br>';
-                        previous_post_link();
-                        ?>
-
+                        </div>
                     </div>
-                    <?php if (comments_open()): ?>
-                    <div class="col-md-8">
-                        <?php comments_template(); ?>
-                    </div>
-                    <?php endif; ?>
                 </div>
-
             </div>
         </div>
-
-        <?php
-    } ?>
-
-    <div class="container post-paginate">
-        <div class="row">
-            <div class="col-md-4"></div>
-            <div class="col-md-8">
-                <?php
-                the_posts_pagination(array(
-                    'screen_reader_text'    => ' ',
-                    'prev_text' =>'New Posts',
-                    'next_text' =>'Old Posts'
-                ));
-                ?>
-            </div>
+        <div class="col-md-4">
+            <?php if (is_active_sidebar('sidebar-1')){
+                dynamic_sidebar('sidebar-1');
+            } ?>
         </div>
     </div>
 </div>
