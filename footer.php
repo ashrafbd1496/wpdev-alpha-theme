56bf2b40a7457db8a21b2a2533ff1734ba13c744
ashrafuddin1496 <asraf1496@gmail.com>
2021-05-26 22:22:29 +0600

    footer widget added

--- footer.php
+++ footer.php
@@ -1,8 +1,15 @@
 <div class="footer">
     <div class="container">
         <div class="row">
-            <div class="col-md-12">
-                &copy; <a href="https://ashrafbd.com">Ashrafbd</a> - All Rights Reserved
+            <div class="col-md-6">
+                <?php if (is_active_sidebar('footer-left')){
+                    dynamic_sidebar('footer-left');
+                } ?>
+            </div>
+            <div class="col-md-6">
+                <?php if (is_active_sidebar('footer-right')){
+                    dynamic_sidebar('footer-right');
+                } ?>
             </div>
         </div>
     </div>
