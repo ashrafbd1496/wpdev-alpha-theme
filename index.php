92ed283798d3bd46ffbcdc54e59602fc400915dd
ashrafuddin1496 <asraf1496@gmail.com>
2021-05-28 21:42:47 +0600

    external javascript enque and lightbox/popup show

--- index.php
+++ index.php
@@ -29,12 +29,18 @@
                     </div>
                     <div class="col-md-8">
                         <p>
-                           <?php
-                           if ( has_post_thumbnail() ) {
-                               the_post_thumbnail('large', array('class' => 'img-fluid'));
+                            <?php
+                            if ( has_post_thumbnail() ) {
+
+                                $thumbnail_url = get_the_post_thumbnail_url(null,'large');
+//                                            echo '<a href="'.$thumbnail_url.'" data-featherlight="image">';
+                                printf('<a href="%s" data-featherlight="image">',$thumbnail_url);
+
+                                the_post_thumbnail('large', array('class' => 'img-fluid'));
 
                             }
-                           ?>
+                            echo'</a>';
+                            ?>
                         </p>
                         <?php
                             the_excerpt();
--- single.php
+++ single.php
@@ -34,9 +34,15 @@
                                     <p>
                                         <?php
                                         if ( has_post_thumbnail() ) {
-                                            the_post_thumbnail('large', array('class' => 'img-fluid'));
+
+                                            $thumbnail_url = get_the_post_thumbnail_url(null,'large');
+//                                            echo '<a href="'.$thumbnail_url.'" data-featherlight="image">';
+                                            printf('<a href="%s" data-featherlight="image">',$thumbnail_url);
+
+                                           the_post_thumbnail('large', array('class' => 'img-fluid'));
 
                                         }
+                                        echo'</a>';
                                         ?>
                                     </p>
                                     <?php
