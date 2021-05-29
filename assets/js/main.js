03f62fb2560f5a7c27a12a0db7856bd0f3c5b49a
ashrafuddin1496 <asraf1496@gmail.com>
2021-05-29 22:37:24 +0600

    image url/href added using jquery

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,1 +1,8 @@
-alert('alllah');
+(function($){
+    $(document).ready(function(){
+        $('.popup').each(function(){
+            var image = $(this).find('img').attr('src');
+            $(this).attr('href',image);
+        });
+    });
+})(jQuery);
