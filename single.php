740b1554ea6d1aae8a3bed39db459266ba35bb11
ashrafuddin1496 <asraf1496@gmail.com>
2021-06-06 13:25:35 +0600

    show author section in single page done

--- single.php
+++ single.php
@@ -54,6 +54,21 @@
                                     ?>
 
                                 </div>
+                                <div class="authorsection">
+                                    <div class="row">
+                                        <div class="col-md-2 authorimage">
+                                            <?php echo get_avatar( get_the_author_meta( 'ID' )); ?>
+                                        </div>
+                                        <div class="col-md-10">
+                                            <h4>
+                                                <?php echo get_the_author_meta('display_name'); ?>
+                                            </h4>
+                                            <p>
+                                                <?php echo get_the_author_meta('description'); ?>
+                                            </p>
+                                        </div>
+                                    </div>
+                                </div>
                                 <?php if (comments_open()): ?>
                                     <div class="col-md-8">
                                         <?php comments_template(); ?>
