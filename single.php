e135c338566bbe041b5f67252b60afab2f029d9f
ashrafuddin1496 <asraf1496@gmail.com>
2021-06-06 18:24:18 +0600

    sidebar check -show sidebar if has any widget

--- single.php
+++ single.php
@@ -1,108 +1,116 @@
-<?php get_header();?>
+<?php
+$alpha_layout_class = "col-md-8";
+$alpha_text_class = "";
+if ( !is_active_sidebar( "sidebar-1" ) ) {
+    $alpha_layout_class = "col-md-10 offset-md-1";
+    $alpha_text_class = "text-center";
+}
+?>
 
+<?php get_header(); ?>
 <body <?php body_class(); ?>>
-
-<?php get_template_part('/template-parts/common/hero');?>
-<div class="container">
-    <div class="row">
-        <div class="col-md-8">
-            <div class="posts">
-                <?php while (have_posts()){
-                    the_post();
-                    ?>
-                    <div <?php post_class(); ?>>
-                        <div class="container">
-                            <div class="row">
-                                <div class="col-md-12">
-                                    <h2 class="post-title">
-                                        <?php the_title(); ?>
-                                    </h2>
-                                </div>
-                            </div>
-                            <div class="row">
-                                <div class="col-md-4">
-                                    <p>
-                                        <strong>
-                                            <?php the_author(); ?>
-                                        </strong><br/>
-                                        <?php echo get_the_date();?>
-                                    </p>
-                                    <?php echo get_the_tag_list("<ul class='list-unstyled'><li>","</li><li>","</li></ul>"); ?>
-
-                                </div>
-                                <div class="col-md-8">
-                                    <p>
-                                        <?php
-                                        if ( has_post_thumbnail() ) {
-
-                                            $thumbnail_url = get_the_post_thumbnail_url(null,'large');
-                                            echo '<a class="popup" href="#" data-featherlight="image">';
-                                           the_post_thumbnail('large', array('class' => 'img-fluid'));
-
-                                        }
-                                        echo'</a>';
-                                        ?>
-                                    </p>
-                                    <?php
-                                    the_content();
-
-                                    wp_link_pages();
-
-//                                    next_post_link();
-//                                    echo '</br>';
-//                                    previous_post_link();
-                                    ?>
-
-                                </div>
-                                <div class="authorsection">
-                                    <div class="row">
-                                        <div class="col-md-2 authorimage">
-                                            <?php echo get_avatar( get_the_author_meta( 'ID' )); ?>
-                                        </div>
-                                        <div class="col-md-10">
-                                            <h4>
-                                                <?php echo get_the_author_meta('display_name'); ?>
-                                            </h4>
-                                            <p>
-                                                <?php echo get_the_author_meta('description'); ?>
-                                            </p>
-                                        </div>
-                                    </div>
-                                </div>
-                                <?php if (comments_open()): ?>
-                                    <div class="col-md-8">
-                                        <?php comments_template(); ?>
-                                    </div>
-                                <?php endif; ?>
-                            </div>
-
-                        </div>
-                    </div>
-
+<?php get_template_part( "/template-parts/common/hero" ); ?>
+    <div class="container">
+        <div class="row">
+            <div class="<?php echo $alpha_layout_class; ?>">
+                <div class="posts" <?php ?>>
                     <?php
-                } ?>
+                    while ( have_posts() ) :
+                        the_post();
+                        ?>
+                        <div <?php post_class(); ?>>
+                            <div class="container">
+                                <div class="row">
+                                    <div class="col-md-12">
+                                        <h2 class="post-title <?php echo $alpha_text_class; ?>">
+                                            <?php the_title(); ?>
+                                        </h2>
+                                        <p class="<?php echo $alpha_text_class; ?>">
+                                            <em><?php the_author(); ?></em><br/>
+                                            <?php echo get_the_date(); ?>
+                                        </p>
+                                    </div>
+                                </div>
+                                <div class="row">
+                                    <div class="col-md-12">
+                                        <p>
+                                            <?php
+                                            if ( has_post_thumbnail() ) {
+                                                $thumbnail_url = get_the_post_thumbnail_url( null, "large" );
+                                                printf( '<a class="popup" href="%s" data-featherlight="image">', $thumbnail_url );
+                                                the_post_thumbnail( "large", array( "class" => "img-fluid" ) );
+                                                echo '</a>';
+                                            }
 
-                <div class="container post-paginate">
-                    <div class="row">
-                        <div class="col-md-4"></div>
-                        <div class="col-md-8">
-                            <?php
-                            the_posts_pagination(array(
-                                'screen_reader_text'    => ' ',
-                                'prev_text' =>'New Posts',
-                                'next_text' =>'Old Posts'
-                            ));
-                            ?>
+                                            the_content();
+
+                                            wp_link_pages();
+
+                                            ?>
+                                        </p>
+                                    </div>
+
+                                    <div class="authorsection">
+                                        <div class="row">
+                                            <div class="col-md-2 authorimage">
+                                                <?php
+                                                echo get_avatar( get_the_author_meta( "ID" ) );
+                                                ?>
+                                            </div>
+                                            <div class="col-md-10">
+                                                <h4>
+                                                    <?php echo get_the_author_meta( "display_name" ); ?>
+                                                </h4>
+                                                <p>
+                                                    <?php echo get_the_author_meta( "description" ); ?>
+                                                </p>
+
+                                            </div>
+                                        </div>
+                                    </div>
+
+                                    <?php if ( comments_open() ): ?>
+                                        <div class="col-md-12">
+                                            <?php
+                                            //comments_template();
+                                            ?>
+                                        </div>
+                                    <?php endif; ?>
+                                </div>
+                            </div>
+                        </div>
+                    <?php
+                    endwhile;
+                    ?>
+
+                    <div class="container post-pagination">
+                        <div class="row">
+                            <div class="col-md-4"></div>
+                            <div class="col-md-8">
+                                <?php
+                                the_posts_pagination( array(
+                                    "screen_reader_text" => ' ',
+                                    "prev_text"          => "New Posts",
+                                    "next_text"          => "Old Posts"
+                                ) );
+                                ?>
+                            </div>
                         </div>
                     </div>
                 </div>
             </div>
-        </div>
-        <div class="col-md-4">
-            <?php if (is_active_sidebar('sidebar-1')){
-                dynamic_sidebar('sidebar-1');
-            } ?>
+            <?php
+            if ( is_active_sidebar( "sidebar-1" ) ):
+                ?>
+                <div class="col-md-4">
+                    <?php
+                    if ( is_active_sidebar( "sidebar-1" ) ) {
+                        dynamic_sidebar( "sidebar-1" );
+                    }
+                    ?>
+                </div>
+            <?php endif; ?>
         </div>
     </div>
-</div>
-<?php get_footer();?>
+
+<?php get_footer(); ?>
