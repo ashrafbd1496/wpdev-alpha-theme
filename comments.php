a22eca5b4e48cb6ad4a699e758ef240855a770b5
ashrafuddin1496 <asraf1496@gmail.com>
2021-06-15 11:15:01 +0600

    show comment heading and comments number

--- comments.php
+++ comments.php
@@ -1,6 +1,24 @@
-<?php
-wp_list_comments();
-if(! comments_open()){
-    _e('Comments are closed', 'alpha');
-}
-comment_form();
+<div class="comments mt-3">
+    <h3>
+        <?php
+        $alpha_cn = get_comments_number();
+        if (1 == $alpha_cn){
+            _e(' 1 Comments','alpha');
+        }else{
+            echo $alpha_cn .' '.__('Comments','alpha');
+        }
+
+        ?>
+    </h3>
+    <?php
+    wp_list_comments();
+    if(! comments_open()){
+        _e('Comments are closed', 'alpha');
+    }
+    comment_form();
+    ?>
+
+</div>
+
+
+
