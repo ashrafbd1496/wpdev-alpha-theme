1d26afbf6cf79709ccae1d498e983b34230145e8
ashrafuddin1496 <asraf1496@gmail.com>
2021-06-16 12:18:37 +0600

    show list of comments and pagination using wp_list_comments

--- comments.php
+++ comments.php
@@ -1,23 +1,38 @@
 <div class="comments mt-3">
-    <h3>
+    <h3 class="comments-title">
         <?php
         $alpha_cn = get_comments_number();
-        if (1 == $alpha_cn){
-            _e(' 1 Comments','alpha');
-        }else{
-            echo $alpha_cn .' '.__('Comments','alpha');
+        if (1 == $alpha_cn) {
+            _e(' 1 Comments', 'alpha');
+        } else {
+            echo $alpha_cn . ' ' . __('Comments', 'alpha');
         }
 
         ?>
     </h3>
-    <?php
-    wp_list_comments();
-    if(! comments_open()){
-        _e('Comments are closed', 'alpha');
-    }
-    comment_form();
-    ?>
+    <div class="comments-list">
+        <?php
+        wp_list_comments();
+        ?>
+        <div class="comments-pagination">
+            <?php the_comments_pagination(array(
+                'screen_reader_text'    => __('Pagination','alpha'),
+                'prev_text' => "<" . __('Previous Comments','alpha'),
+                'prev_text' => ">" . __('Next Comments','alpha')
 
+            )); ?>
+        </div>
+        <?php
+        if (!comments_open()) {
+            _e('Comments are closed', 'alpha');
+        }
+        ?>
+        <div class="comments-form">
+            <?php
+            comment_form();
+            ?>
+        </div>
+    </div>
 </div>
 
 
