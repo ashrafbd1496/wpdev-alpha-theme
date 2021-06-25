6360a62e09e0dc7c8f03fddf2ca5025e9a18adf2
ashrafuddin1496 <asraf1496@gmail.com>
2021-06-25 08:44:49 +0600

    use of WP_Query class inside custom query

--- cq-wpq.php
+++ cq-wpq.php
@@ -1,6 +1,6 @@
 <?php
 /**
- * Template Name: Custom Query
+ * Template Name: Custom Query WPQuery
  */
 
 ?>
@@ -12,35 +12,30 @@
     $paged = get_query_var('paged') ? get_query_var('paged'):1;
     $posts_per_page = 2;
     $total = 9;
-    $post_ids = array(23,28,17,42,25,249);
-    $cp = get_posts(array(
-            'posts_per_page' => $posts_per_page,
+    $post_ids = array(15,23,28,15,25,249);
+    $cp = new WP_Query(array(
+           //'posts_per_page' => $posts_per_page,
             'number_of_posts'   => $total,
             'author__in'    => array(1),
-//            'post__in'   =>  $post_ids,
+           'post__in'   =>  $post_ids,
 //        'order' =>'asc',
             'orderby'   => 'post__in',      //to show order exactly we want
             'paged' => $paged,
     ));
-
-    foreach ($cp as $post){
-      setup_postdata($post);
+    while ( $cp->have_posts() ) {
+        $cp->the_post();
         ?>
         <h5><a href="<?php the_permalink(); ?>"><?php the_title(); ?></a></h5>
-
         <?php
-
     }
-    wp_reset_postdata();
+    wp_reset_query();
     ?>
     <div class="container post-paginate">
         <div class="row">
             <div class="col-md-4"></div>
             <div class="col-md-8">
                 <?php
-                echo paginate_links( array(
-                    'total' => ceil( $total / $posts_per_page )
-                ) );
+
                 ?>
             </div>
         </div>
